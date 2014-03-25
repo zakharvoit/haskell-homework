9684274ee0fb60645b4a3d64014692bc811f6289
Zakhar Voit <zakharvoit@gmail.com>
2014-03-25 19:40:30 +0400

    Added Vector and Number typeclasses and some pointless magic

--- Functional.hs
+++ Functional.hs
@@ -1,26 +1,37 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 
-module Functional(($), at, (.), (.>), on, ($$)) where
+module Functional(flip,join, ($), at, (.), (.>), on, ($$)) where
 
 infixr 0 $
 ($) :: (a -> b) -> (a -> b)
-f $ x = f x
+-- f $ x = f x
+($) f = f
 
 at :: a -> (a -> b) -> b
-x `at` f = f x
+at x f = f x
   
 infixr 9 .
 (.) :: (b -> c) -> (a -> b) -> (a -> c)
 (.) f g x = f (g x)
 
 -- In default library: >>>
-(.>) :: (c -> d) -> (a -> c) -> (a -> d)
-g .> f = g . f
+(.>) :: (a -> c) -> (c -> d) -> (a -> d)
+g .> f = f . g
 
 infixl 0 `on`
 on :: (b -> b -> c) -> (a -> b) -> a -> a -> c
-on f g x y = f (g x) (g y)
+-- on f g x y = f (g x) (g y)
+-- on f g = ((((. (f .)) . (.)) . ((flip (.))))) g g
+on = join . ((. ((flip (.)))) . ((. (.)) . ((flip (.)) . (.))))
 
 ($$) :: (c -> d) -> (a -> b -> c) -> (a -> b -> d)
-($$) g f x y = g $ f x y
+-- ($$) g f x y = g $ f x y
+-- ($$) g f x = g . (f x)
+-- ($$) g f = (g .) . f
+($$) = (.) . (.)
 
+join :: (a -> a -> b) -> a -> b
+join f x = f x x
+
+flip :: (a -> b -> c) -> (b -> a -> c)
+flip f x y = f y x
--- Vector.hs
+++ Vector.hs
@@ -1,61 +1,89 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 
-module Main where
+module Vector where
+
+import Prelude(Show)
 
 import Base
 import Functional
 
-class Vector v where
+class LikeVector v where
   map :: (a -> b) -> v a -> v b
   zip :: (a -> b -> c) -> v a -> v b -> v c
   fold :: (a -> a -> a) -> v a -> a
 
-class Number a where
+class LikeNumber a where
   (+) :: a -> a -> a
   (*) :: a -> a -> a
 
-instance Number Int where
+instance LikeNumber Int where
   (+) = sumInt
   (*) = mulInt
 
-instance Number Float where
+instance LikeNumber Float where
   (+) = sumFloat
   (*) = mulFloat
-  
-data Vector2D a = Vector2D a a
 
-instance Vector Vector2D where
+instance LikeNumber Double where
+  (+) = sumDouble
+  (*) = mulDouble
+
+data LikeVector2D a = LikeVector2D
+                  { vx2D :: a,
+                    vy2D :: a
+                  } deriving Show
+
+instance LikeVector LikeVector2D where
   map = map2D
   zip = zip2D
   fold = fold2D
 
-map2D :: (a -> b) -> Vector2D a -> Vector2D b
-map2D f (Vector2D x y) = (Vector2D `on` f) x y
+map2D :: (a -> b) -> LikeVector2D a -> LikeVector2D b
+-- map2D = join . ((. vy2D) .) . (. vx2D) . (LikeVector2D `on`)
+-- map2D f = (vy2D `at`) . (vx2D `at`) . (on LikeVector2D) . ((f .) . at)
+map2D = (((at vy2D) . (at vx2D) . (on LikeVector2D)) .) . (((. at) . (.)))
+-- map2D f v = (LikeVector2D `on` f) (vx v) (vy v)
 
-zip2D :: (a -> b -> c) -> Vector2D a -> Vector2D b -> Vector2D c
-zip2D f (Vector2D x y) (Vector2D x' y') = f y y' `at` Vector2D $ f x x'
+zip2D :: (a -> b -> c) -> LikeVector2D a -> LikeVector2D b -> LikeVector2D c
+-- zip2D f (LikeVector2D x y) (LikeVector2D x' y') = LikeVector2D (f x x') $ f y y'
+-- zip2D f v v' = LikeVector2D (f (vx2D v) (vx2D v')) (f (vy2D v) (vy2D v'))
+zip2D f = join . (join $ (. (LikeVector2D .) . (. vx2D) . f . vx2D) . (.) . (flip (.)) . (. vy2D) . (f . vy2D))
 
-fold2D :: (a -> a -> a) -> Vector2D a -> a
-fold2D f (Vector2D x y) = f x y
 
-data Vector3D a = Vector3D a a a
+fold2D :: (a -> a -> a) -> LikeVector2D a -> a
+-- fold2D f (LikeVector2D  y) = f x y
+-- fold2D f v = ((. vy2D) . (f . vx2D)) v v
+-- fold2D = join . (((. vy2D) .) . (. vx2D))
+-- fold2D f v = f (vx2D v) (vy2D v)
+fold2D f = (((vy2D `at`) . (vx2D `at`)) . (on f) . at)
 
-instance Vector Vector3D where
+
+data LikeVector3D a = LikeVector3D
+                  { vx3D :: a,
+                    vy3D :: a,
+                    vz3D :: a
+                  }
+                deriving Show
+
+instance LikeVector LikeVector3D where
   map = map3D
   zip = zip3D
   fold = foldr3D
  
-map3D :: (a -> b) -> Vector3D a -> Vector3D b
-map3D f (Vector3D x y z) = f z `at` (Vector3D `on` f) x y
+map3D :: (a -> b) -> LikeVector3D a -> LikeVector3D b
+map3D f (LikeVector3D x y z) = f z `at` (LikeVector3D `on` f) x y
 
-zip3D :: (a -> b -> c) -> Vector3D a -> Vector3D b -> Vector3D c
-zip3D f (Vector3D x y z) (Vector3D x' y' z') = Vector3D (f x x') (f y y') (f z z')
+zip3D :: (a -> b -> c) -> LikeVector3D a -> LikeVector3D b -> LikeVector3D c
+zip3D f (LikeVector3D x y z) (LikeVector3D x' y' z') = LikeVector3D (f x x') (f y y') (f z z')
 
-foldr3D :: (a -> a -> a) -> Vector3D a -> a
-foldr3D f (Vector3D x y z) = f x $ f y z
+foldr3D :: (a -> a -> a) -> LikeVector3D a -> a
+-- foldr3D f (LikeVector3D x y z) = f x $ f y z
+foldr3D f v = ((f . vx3D) v) $ ((. vz3D) . f . vy3D) v v
 
-foldl3D :: (a -> a -> a) -> Vector3D a -> a
-foldl3D f (Vector3D x y z) = f x y `f` z
+foldl3D :: (a -> a -> a) -> LikeVector3D a -> a
+foldl3D f (LikeVector3D x y z) = f x y `f` z
+-- foldl3D f v = f (f (vx3D v) (vy3D v)) (vz3D z)
 
-innerProduct :: (Vector v, Number a) => v a -> v a -> a
+innerProduct :: (LikeVector v, LikeNumber a) => v a -> v a -> a
 innerProduct = fold (+) $$ zip (*)
+-- innerProduct = ((fold (+)) .) . (zip (*))
